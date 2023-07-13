ccabdf296dcf7ab0a7782753816f0e81bb85e6f4
Karan Raj <120357737+karan2198@users.noreply.github.com>
2023-07-13 09:22:28 +0530

    Time: 25 ms (62.83%), Space: 13.9 MB (46.36%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-      bool DFSRec(int s, vector<bool>&visited, vector<bool>&currVisited, vector<int>adj[]){
-        visited[s] = true;
-        currVisited[s] = true;
-        
-        vector<int>data = adj[s];
-        for(auto x: data){
-            if(!visited[x]){
-                if(DFSRec(x, visited, currVisited, adj)){
-                    return true;
-                }
-            }
-            else if(visited[x] && currVisited[x]){
-                return true;
-            }
-        }
-        
-        currVisited[s] = false;
-        return false;
-    }
-    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-          int n = numCourses;
-        vector<bool>visited(n, false), currVisited(n, false);
-        
-        vector<int>adj[n];
-        for(auto x: prerequisites){
-            vector<int>data = x;
-            int a = data[0];
-            int b = data[1];
-            adj[a].push_back(b);
+        vector<int> indegree(numCourses);
+        vector<vector<int>> adj(numCourses);
+        for (auto prerequisite : prerequisites) {
+            adj[prerequisite[1]].push_back(prerequisite[0]);
+            indegree[prerequisite[0]]++;
         }
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                if(DFSRec(i, visited, currVisited, adj)){
-                    return false;
+
+        queue<int> q;
+        // Push all the nodes with indegree zero in the queue.
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int nodesVisited = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            nodesVisited++;
+
+            for (auto& neighbor : adj[node]) {
+                // Delete the edge "node -> neighbor".
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
                 }
             }
         }
-        
-        return true;
+
+        return nodesVisited == numCourses;
     }
 };
